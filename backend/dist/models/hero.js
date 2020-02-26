6dd332446f14dd5af9b2489c8dca3848d1f76d96
Axel Saredo <apsaredo@gmail.com>
2020-02-25 22:01:19 -0300

    Use db data for routing

---
+++ backend/dist/models/hero.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Hero {
+    constructor(id, name, superpowers, weaknesses, img) {
+        this.id = id;
+        this.name = name;
+        this.superpowers = superpowers;
+        this.weaknesses = weaknesses;
+        this.img = img;
+    }
+}
+exports.Hero = Hero;
--- backend/src/models/hero.model.ts
+++ backend/src/models/hero.model.ts
@@ -1,9 +1,18 @@
-export class Hero {
-  constructor(
-    public id: string,
-    public name: string,
-    public superpowers: string[],
-    public weaknesses: string[],
-    public img: string
-  ) {}
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface IHero extends Document {
+  name: string;
+  superpowers: string[];
+  weaknesses: string[];
+  img: string;
 }
+
+export const HeroSchema = new Schema({
+  name: { type: String, required: true },
+  superpowers: { type: Array, required: true },
+  weaknesses: { type: Array, required: true },
+  img: { type: String, required: true }
+});
+
+const Hero = mongoose.model<IHero>('Hero', HeroSchema);
+export default Hero;
